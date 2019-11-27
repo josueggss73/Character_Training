df6c4a3333a7f4e6b5313a1bcb8f15d2099d4da5
marcasro9 <marcasro9@gmail.com>
2019-11-27 00:36:35 -0600

    New Concrete class implements.

--- src/Model/CharacterAffector/IStateListing.java
+++ src/Model/CharacterAffector/IStateListing.java
@@ -8,4 +8,5 @@ public interface IStateListing extends IPrototype<IStateListing> {
     IState getState(int index);
     void addState(IState state);
     void deleteState(States state);
+    void deleteAllStates();
 }
