91f2f5f8ba467056e85e61ae8a5f3b7148c80efa
Paulo <paulozuniga17@gmail.com>
2019-11-27 07:40:38 -0600

    Memento started

--- src/Model/LogArray.java
+++ src/Model/LogArray.java
@@ -1,6 +1,7 @@
 package Model;
 
 import Model.CharacterAffector.CharacterStrategies.ILog;
+import Patterns.IPrototype;
 
 import java.util.ArrayList;
 
@@ -24,5 +25,19 @@ public class LogArray implements ILogListing {
     @Override
     public void addLog(ILog log) {
         logs.add(log);
+    }
+
+    @Override
+    public IPrototype clone() {
+        return null;
+    }
+
+    @Override
+    public IPrototype deepClone() {
+        ArrayList<ILog> clonedLogs = new ArrayList<>();
+        for(int i=0; i<logs.size(); i++){
+            clonedLogs.add((ILog) logs.get(i).deepClone());
+        }
+        return (IPrototype) clonedLogs;
     }
 }
